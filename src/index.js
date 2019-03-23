88797551a2db951291d62d245b4cdfdc46b29632
sakaviec <sakaviec@users.noreply.github.com>
2019-03-24 02:54:42 +0300

    feat: implement love triangles formula

--- src/index.js
+++ src/index.js
@@ -3,5 +3,34 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let population = [undefined];
+  let result = 0;
+
+  for (let i = 0; i < preferences.length; i++) {
+    population[i+1] = preferences[i];
+  }
+
+  for (let oneWhoLoves = 1; oneWhoLoves <= preferences.length; oneWhoLoves++) {
+
+    let oneWhoIsLoved = population[oneWhoLoves];
+
+    for (let j = 1; j <= preferences.length; j++) {
+        let oneWhoIsThird = population[oneWhoIsLoved];
+
+        if (population[oneWhoIsThird]==oneWhoLoves) {
+            //console.log(oneWhoLoves+" - "+oneWhoIsLoved+" — "+oneWhoIsThird);
+            result++
+        }
+            else {}
+    }
+
+
+  }
+  
+  if (preferences.length===0) {
+  	return 0
+  }
+  	else{
+  		return Math.floor(result/preferences.length/3)
+  	}
 };
